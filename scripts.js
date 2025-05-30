6a863e62dd310b822a76879cb7375e30de2cfa6d
VaishnaviY-03 <vaishnaviyaragarla1003@gmail.com>
2025-05-30 19:48:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.read-more').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("Redirecting to story page...");
+        // You can add real navigation or load more content here
+    });
+});
